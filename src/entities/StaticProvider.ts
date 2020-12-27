b02bbae8b78b7c56990ab39e284b1ac18ab28438
Anton Malofeev <arenukvern@gmail.com>
2020-12-27 05:30:12 +0300

    Clean up

--- src/entities/StaticProvider.ts
+++ src/entities/StaticProvider.ts
@@ -16,19 +16,15 @@ interface SaveToStorageArg<S> {
  */
 export const saveToStorage = <S, T>(arg: SaveToStorageArg<S>) => {
   const { state, storageName } = arg
-  console.log('saveStorage Wrapper', arg)
   return (
     target: StaticProviderStorage<S>,
     propertyKey: string,
     descriptor?: Maybe<TypedPropertyDescriptor<T>>
   ) => {
-    console.log('saveStorage descriptor', descriptor)
-
     if (descriptor) {
       const set = descriptor.set
       descriptor.set = function(...arg) {
         if (set == undefined) throw Error(`set ${propertyKey} is not defined!`)
-        console.log('saving')
 
         set.call(target, ...arg)
         localStorage.setItem(storageName, JSON.stringify(state))
