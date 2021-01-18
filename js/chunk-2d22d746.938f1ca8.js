7e05519797c1a8273c05f8d56d2e872f3bf01669
Arenukvern <arenukvern@gmail.com>
2021-01-18 06:39:00 +0000

    update

---
+++ js/chunk-2d22d746.938f1ca8.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,c,t){"use strict";t.r(c);var e=t("7a23"),a={class:"about"},s=Object(e["g"])("h1",null,"This is an about page",-1);function u(n,c){return Object(e["r"])(),Object(e["c"])("div",a,[s])}const o={};o.render=u;c["default"]=o}}]);
---
+++ precache-manifest.ff360475e09708ee6d0b9e50505cb35a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/cell_editor_excel_addin/assets/icon.png"
+  },
+  {
+    "revision": "58059ef6885059ca39665b16fa156598",
+    "url": "/cell_editor_excel_addin/commands/commands.html"
+  },
+  {
+    "revision": "9c01037d4f94508e7e6faa56e3121c32",
+    "url": "/cell_editor_excel_addin/commands/commands.js"
+  },
+  {
+    "revision": "c9e3722957850e095f76",
+    "url": "/cell_editor_excel_addin/css/app.68f033ed.css"
+  },
+  {
+    "revision": "2727e00786a6605de5f9",
+    "url": "/cell_editor_excel_addin/css/chunk-0bd2984e.c0d93c21.css"
+  },
+  {
+    "revision": "5b8f688da274e59a5a96",
+    "url": "/cell_editor_excel_addin/css/chunk-6995e580.a13b6298.css"
+  },
+  {
+    "revision": "2e249327c5214ee29b57ed2a6dca8806",
+    "url": "/cell_editor_excel_addin/index.html"
+  },
+  {
+    "revision": "c9e3722957850e095f76",
+    "url": "/cell_editor_excel_addin/js/app.ecb5d5f1.js"
+  },
+  {
+    "revision": "2727e00786a6605de5f9",
+    "url": "/cell_editor_excel_addin/js/chunk-0bd2984e.19727ab7.js"
+  },
+  {
+    "revision": "7cd20f47f6a278ec1ce3",
+    "url": "/cell_editor_excel_addin/js/chunk-2d0c7b4f.5d579d6a.js"
+  },
+  {
+    "revision": "ecb8276ef309b9fe04a7",
+    "url": "/cell_editor_excel_addin/js/chunk-2d0de507.b34d2855.js"
+  },
+  {
+    "revision": "bec62ac1c22d2435c0c1",
+    "url": "/cell_editor_excel_addin/js/chunk-2d20e7d9.430322ce.js"
+  },
+  {
+    "revision": "6191048a9dc544587772",
+    "url": "/cell_editor_excel_addin/js/chunk-2d21df96.5b096225.js"
+  },
+  {
+    "revision": "7601f4d3fe8c5fe19a3c",
+    "url": "/cell_editor_excel_addin/js/chunk-2d22d746.938f1ca8.js"
+  },
+  {
+    "revision": "5b8f688da274e59a5a96",
+    "url": "/cell_editor_excel_addin/js/chunk-6995e580.893c8206.js"
+  },
+  {
+    "revision": "8d78741476b5abfbdb56",
+    "url": "/cell_editor_excel_addin/js/chunk-7965dcc0.69d5fbf6.js"
+  },
+  {
+    "revision": "d0589650cee77d0cb48d",
+    "url": "/cell_editor_excel_addin/js/chunk-b3b0c4a0.a71740a6.js"
+  },
+  {
+    "revision": "4ceccf7dbc15cb0a9c4a",
+    "url": "/cell_editor_excel_addin/js/chunk-vendors.7e942aab.js"
+  },
+  {
+    "revision": "6943f42bfa712a716e33817230c48a27",
+    "url": "/cell_editor_excel_addin/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/cell_editor_excel_addin/robots.txt"
+  }
+]);
